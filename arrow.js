842b28902a09d062aa72516cd5efda6da597d889
Tara Denniston <taradenniston@gmail.com>
2022-07-21 17:19:31 -0400

    initial commit

---
+++ arrow.js
@@ -0,0 +1,29 @@
+// Refactor the code to use two arrow functions. Turn it into a one-liner.
+
+// ES5 Map Callback
+// function double(arr) {
+//   return arr.map(function(val) {
+//     return val * 2;
+//   });
+// }
+
+// ES2015 Version
+
+
+
+
+// Refactor the following function to use arrow functions:
+// Replace ALL functions with arrow functions:
+
+// function squareAndFindEvens(numbers){
+//   var squares = numbers.map(function(num){
+//     return num ** 2;
+//   });
+//   var evens = squares.filter(function(square){
+//     return square % 2 === 0;
+//   });
+//   return evens;
+// }
+
+// ES2015 Version
+
